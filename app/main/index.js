622afec3bcdc3be9e3a4995ca1439bc29542945c
Jason Garber <jason@sixtwothree.org>
2017-01-18 09:43:19 -0500

    Remove unnecessary assignments.

--- app/main/index.js
+++ app/main/index.js
@@ -3,10 +3,7 @@ const {app} = require('electron');
 const MainMenu = require('./menu');
 const MainWindow = require('./window');
 
-let mainMenu;
-let mainWindow;
-
 app.on('ready', () => {
-	mainMenu = new MainMenu();
-	mainWindow = new MainWindow();
+	new MainMenu();
+	new MainWindow();
 });
